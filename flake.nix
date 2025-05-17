3cf765328d483d2c385902403f376d71128f75b8
WuX <wux@thenest.dev>
2025-05-17 11:27:36 +0300

    chore: add package to flake

--- flake.nix
+++ flake.nix
@@ -4,12 +4,26 @@
         flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
         let pkgs = import nixpkgs { inherit system; }; in {
             devShells.default = pkgs.mkShell {
                 shellHook = "export NIX_SHELL_NAME='fabricate'";
                 buildInputs = with pkgs; [ go sphinx python312Packages.sphinx-rtd-theme ];
                 nativeBuildInputs = with pkgs; [ ninja ];
+            };
+
+            defaultPackage = pkgs.buildGoModule {
+                name = "fabricate";
+                src = self;
+
+                vendorHash = "sha256-xY/VVT/KFEbipLTPXUr5EM+uCO1E9KHvLy74LtFusWM=";
+
+                meta = {
+                    description = " Simple yet powerful meta buildsystem.";
+                    homepage = "https://github.com/elysium-os/fabricate";
+                    license = pkgs.lib.licenses.bsd3;
+                    maintainers = with pkgs.lib.maintainers; [ wux ];
+                };
             };
         }
     );
